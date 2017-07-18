c9d62f5dca0c1b64ac0411ad5b8b108a78a352d9
Paulina Wątroba <paulina@watroba.com.pl>
2017-07-18 08:50:04 +0200

    Insert działa do tabeli Pracownicy działa  :)

--- Workspace/Program.cs
+++ Workspace/Program.cs
@@ -11,56 +11,64 @@ namespace Workspace
     class Program
     {
         static void Main(string[] args)
-        {    //
+        {    // Inicjalizacja nowego obiektu odpowiedzialnego za zapis do pliku
+            
             StreamWriter sw = new StreamWriter(@"C:\Users\praktykant\Documents\nowyplik.txt", false);
-
-
-
+            
+            /*
+             * Zmienne do Connection String
+            */
             string instance = @"ELPLC-0305\SQLEXPRESS";
             string dbdir = "Pracownicy";
             string id= "sa";
             string password = "Pr4ktyk4nt1!";
-            //Console.WriteLine(password);
-
-            //Console.WriteLine("Hello world");
-
+            
             /*Connection String:
              * zawiera dane serwera SQL, z którym się łączymy:
              * nazwę instancji - Data Source
              * dane konta: User ID / Password
              * Nazwa Bazy Danych-Initial Catalog
              */
-            SqlConnection sqlConn = new SqlConnection("Data Source=" + instance + ";"+"User ID=" + id + ";"+"Password="+ password+";" + "Initial Catalog=" + dbdir+";");
+                      SqlConnection sqlConn = new SqlConnection("Data Source=" + instance + ";"+"User ID=" + id + ";"+"Password="+ password+";" + "Initial Catalog=" + dbdir+";");
 
-            //Console.WriteLine(sqlConn.ConnectionString);
-            //sw.WriteLine(sqlConn.ConnectionString);
             try
-            {
-                
+            {               
                 // otwórz połączenie:
                 sqlConn.Open();
-
+                DateTime date=DateTime.Now;
                 Console.WriteLine("Połączono z bazą danych!");
-
-               
                 
-               // Console.ReadLine();
-                // 1. inicjujemy nowe polecenie wraz z zapytaniem i połączeniem
-                //SqlCommand cmd = new SqlCommand("select * from dbo.SIEDZIBY", sqlConn);
-                SqlCommand cmd = new SqlCommand("insert into dbo.STANOWISKA values (@id,@nazwa)",sqlConn);
+                SqlCommand cmd = new SqlCommand("insert into dbo.PRACOWNICY values (@id,@imie,@nazwisko,@data,@ids,@idd)", sqlConn);
+                cmd.Parameters.AddWithValue("@id", "1");
+                cmd.Parameters.AddWithValue("@imie", "Jan");
+                cmd.Parameters.AddWithValue("@nazwisko", "Kowalski");
+                cmd.Parameters.AddWithValue("@data", date);
+                cmd.Parameters.AddWithValue("@ids", "1");
+                cmd.Parameters.AddWithValue("@idd", "1");
+                // Inicjalizacja nowego polecenia wraz z zapytaniem i połączeniem
+                /*
+                SqlCommand cmd = new SqlCommand("insert into dbo.DZIALY values (@id,@nazwa,@ids)", sqlConn);
                 cmd.Parameters.AddWithValue("@id", 1);
-                cmd.Parameters.AddWithValue("@nazwa", "PROGRAMISTA");
-             /* 
-              * SqlCommand cmd = new SqlCommand("insert into dbo.PRACOWNICY values (@id,@imie,@nazwisko,@data,@ids,@idd)", sqlConn);
+                cmd.Parameters.AddWithValue("@nazwa", "IT");
+                cmd.Parameters.AddWithValue("@ids", 1);
+                
+                    SqlCommand cmd = new SqlCommand("select * from dbo.SIEDZIBY", sqlConn);
+                
+                    Insert do tabeli dbo.STANOWISKA
+                    SqlCommand cmd = new SqlCommand("insert into dbo.STANOWISKA values (@id,@nazwa)",sqlConn);
+                    cmd.Parameters.AddWithValue("@id", 1);
+                    cmd.Parameters.AddWithValue("@nazwa", "PROGRAMISTA");
+            
+                SqlCommand cmd = new SqlCommand("insert into dbo.PRACOWNICY values (@id,@imie,@nazwisko,@data,@ids,@idd)", sqlConn);
                 cmd.Parameters.AddWithValue("@id", "1000");
                 cmd.Parameters.AddWithValue("@imie", "Jan");
                 cmd.Parameters.AddWithValue("@nazwisko", "Kowalski");
                 cmd.Parameters.AddWithValue("@data", "2001-01-12");
-                cmd.Parameters.AddWithValue("@ids", "0");
-                cmd.Parameters.AddWithValue("@idd", "0");
+                cmd.Parameters.AddWithValue("@ids", "1");
+                cmd.Parameters.AddWithValue("@idd", "1");
                 
               */
-               // 2. wywołujemy  odczyt Execute dla pozyskania wyników zapytania
+               // Wywołanie odczytu Execute dla pozyskania wyników zapytania
                 SqlDataReader rdr = cmd.ExecuteReader();
 
                 while (rdr.Read())
